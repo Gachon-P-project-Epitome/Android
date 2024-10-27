7de0887e6f7ed1010f36e227ad454bc4900cfb0b
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-10-27 22:40:41 +0900

    [9] HomeFragment Logo 애니메이션 설정 완료 + Splash gif 설정(크기는 수정해야함)

--- app/src/main/java/com/janob/epitome/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/janob/epitome/presentation/ui/splash/SplashActivity.kt
@@ -1,22 +1,30 @@
 package com.janob.epitome.presentation.ui.splash
 
-import android.os.Bundle
-import com.janob.epitome.presentation.base.BaseActivity
 import android.content.Intent
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.bumptech.glide.Glide
+import com.janob.epitome.R
 import com.janob.epitome.databinding.ActivitySplashBinding
+import com.janob.epitome.presentation.base.BaseActivity
 import com.janob.epitome.presentation.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // GIF 로드
+        Glide.with(this)
+            .asGif()
+            .load(R.raw.epitome)
+            .into(binding.gifLogo)
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 2000)
+        }, 4000)
     }
 }
